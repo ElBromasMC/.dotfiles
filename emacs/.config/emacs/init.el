43359123fad85f46a6aa1d30164a65c29a275d1c
Ander Linares <ander.linares@outlook.com>
2026-01-21 20:39:12 -0500

    sync ElMaquinas

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,14 +1,22 @@
 (setq custom-file (locate-user-emacs-file "custom.el"))
+(load custom-file t)
 
-(pinentry-start)
+(setq backup-directory-alist `(("." . ,(concat user-emacs-directory "backups")))
+      auto-save-file-name-transforms `((".*" ,(concat user-emacs-directory "auto-save/") t))
+      display-line-numbers-type 'relative
+      inhibit-startup-screen t)
+
+(let ((auto-save-dir (concat user-emacs-directory "auto-save/")))
+  (unless (file-exists-p auto-save-dir)
+    (make-directory auto-save-dir t)))
 
 (menu-bar-mode 0)
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
-(ido-mode 1)
+(fido-mode 1)
 (global-display-line-numbers-mode 1)
 
 (load-theme 'modus-vivendi t)
 (add-to-list 'default-frame-alist '(font . "Iosevka-16"))
 
-(load custom-file t)
+(when (fboundp 'pinentry-start) (pinentry-start))
