240cb61431e333e73ad7d6610a396263128ada2d
Ander Linares <ander.linares@outlook.com>
2026-03-28 16:28:06 -0500

    sync maquinas

--- nvim/.config/nvim/lua/elbromasmc/remap.lua
+++ nvim/.config/nvim/lua/elbromasmc/remap.lua
@@ -1,7 +1,7 @@
 -- :h key-notation
-vim.g.mapleader = vim.keycode('<space>')
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
-vim.keymap.set('i', '<C-c>', '<Esc>')
+--vim.g.mapleader = vim.keycode('<space>')
+--vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+--vim.keymap.set('i', '<C-c>', '<Esc>')
 
 -- Remap SQL omni complete
 vim.g.ftplugin_sql_omni_key = '<C-j>'
